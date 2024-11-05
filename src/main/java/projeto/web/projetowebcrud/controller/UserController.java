e33ac91cdd4109832ac262c643027a2819914028
Julio souza <julio_1997_@hotmail.com>
2024-11-05 00:01:33 -0300

    Criado a função 'Criar usuario'

--- src/main/java/projeto/web/projetowebcrud/controller/UserController.java
+++ src/main/java/projeto/web/projetowebcrud/controller/UserController.java
@@ -1,16 +1,28 @@
 package projeto.web.projetowebcrud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import projeto.web.projetowebcrud.entity.User;
+import projeto.web.projetowebcrud.service.UserService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto ){
-        return null;
+       var userId = userService.createUser(createUserDto);
+        return ResponseEntity.created(URI.create("/v1/users" + userId.toString())).build();
     }
 
     @GetMapping("/{userId}")
