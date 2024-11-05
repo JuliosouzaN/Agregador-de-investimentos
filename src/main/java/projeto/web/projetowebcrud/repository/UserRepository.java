fcf350854fbc6b9a9fcd8c179ee8eef2d9c2fac1
Julio souza <julio_1997_@hotmail.com>
2024-11-05 00:00:35 -0300

    Criado o UserRepository e o UserService

---
+++ src/main/java/projeto/web/projetowebcrud/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package projeto.web.projetowebcrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import projeto.web.projetowebcrud.entity.User;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
---
+++ src/main/java/projeto/web/projetowebcrud/service/UserService.java
@@ -0,0 +1,32 @@
+package projeto.web.projetowebcrud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import projeto.web.projetowebcrud.controller.CreateUserDto;
+import projeto.web.projetowebcrud.entity.User;
+import projeto.web.projetowebcrud.repository.UserRepository;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto){
+       var entity = new User(UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null);
+       var userSaved = userRepository.save(entity);
+       return userSaved.getUserId();
+    }
+}
